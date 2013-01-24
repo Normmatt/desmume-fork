8e7db8b4f2c0a5802f67f187e2290e993da79dcc
gechdcb <gechdcb@gmail.com>
2013-01-24 20:25:54 +0800

    android project update

--- desmume/src/android/jni/Android.mk
+++ desmume/src/android/jni/Android.mk
@@ -8,7 +8,7 @@ include $(CLEAR_VARS)
 include $(LOCAL_BUILD_PATH)/cpudetect/cpudetect.mk
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-#include $(LOCAL_BUILD_PATH)/desmume_v7.mk
+include $(LOCAL_BUILD_PATH)/desmume_v7.mk
 include $(LOCAL_BUILD_PATH)/desmume_neon.mk
 else
 include $(LOCAL_BUILD_PATH)/desmume_compat.mk
