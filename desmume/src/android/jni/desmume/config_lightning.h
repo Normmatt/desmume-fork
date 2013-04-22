95a94780a407bd145b49c780761675b0c5c8acaf
gechdcb <gechdcb@gmail.com>
2013-04-22 19:16:51 +0800

    update DeSmuME

---
+++ desmume/src/android/jni/desmume/config_lightning.h
@@ -0,0 +1,15 @@
+#ifndef _CONFIGLIGHTNING_H
+#define _CONFIGLIGHTNING_H
+
+//#define LIGHTNING_I386
+//#define LIGHTNING_X86_64
+#define LIGHTNING_ARM
+//#define LIGHTNING_MIPS
+//#define LIGHTNING_MIPS64
+//#define LIGHTNING_PPC
+//#define LIGHTNING_SPARC
+
+#define __WORDSIZE 32
+//#define __WORDSIZE 64
+
+#endif
