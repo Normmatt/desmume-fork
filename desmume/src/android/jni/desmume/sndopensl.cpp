87d37630c550d31f9b6b1486ad51fd4e7c8a8d36
gechdcb <gechdcb@gmail.com>
2013-06-16 09:26:03 +0800

    Revert "up"
    This reverts commit 648c8fe87b6eeb21361aa034fe75d92cbad3a3b5.

--- desmume/src/android/jni/desmume/sndopensl.cpp
+++ desmume/src/android/jni/desmume/sndopensl.cpp
@@ -87,8 +87,8 @@ SoundBuffer empty;
 
 static bool muted = false;
 static bool currentlyPlaying = false;
-static volatile int soundbufsize = 0;
-static volatile int nextSoundBuffer = -1;
+static int soundbufsize = 0;
+static int nextSoundBuffer = -1;
 static SLmillibel maxVol;
 
 pthread_t soundThread;
